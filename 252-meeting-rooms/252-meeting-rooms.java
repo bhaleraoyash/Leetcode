5d545b63a5247cd649affe0718be04890284db1a
bhaleraoyash <65583711+bhaleraoyash@users.noreply.github.com>
2022-01-03 12:57:01 -0500

    Time: 4 ms (97.29%), Space: 39.1 MB (34.02%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.java
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -1,5 +1,8 @@
 class Solution{
 	public boolean canAttendMeetings(int[][] intervals){
+        if(intervals.length == 0){
+            return true;
+        }
 		Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
 		for(int i = 0; i < intervals.length - 1; i++){
