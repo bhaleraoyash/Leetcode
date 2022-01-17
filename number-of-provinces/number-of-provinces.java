ff83c154d2d6fd8baeaf24ec2092145fbf69462e
bhaleraoyash <65583711+bhaleraoyash@users.noreply.github.com>
2022-01-17 11:01:33 -0500

    Time: 4 ms, Memory: 51 MB - LeetHub

---
+++ number-of-provinces/number-of-provinces.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        UnionFind uf = new UnionFind(isConnected.length);
+        
+        for(int i = 0; i < isConnected.length; i++){
+            for(int j = 0; j < isConnected[i].length; j++){
+                if(isConnected[i][j] == 1 && i != j){
+                    uf.union(i, j);
+                }
+            }
+        }
+        
+        int count = 0;
+        int[] root = uf.returnRoot();
+        for(int i = 0; i < root.length; i++){
+            if(i == root[i]){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
+
+class UnionFind {
+    private int[] root;
+
+    public UnionFind(int size) {
+        root = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            root[rootY] = rootX;
+        }
+    }
+    
+    public int[] returnRoot(){
+        return root;
+    }
+}
