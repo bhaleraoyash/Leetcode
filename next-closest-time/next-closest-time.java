38de136d297f0ae69029340899e3fe7f0557244d
bhaleraoyash <65583711+bhaleraoyash@users.noreply.github.com>
2022-01-24 17:15:56 -0500

    Time: 22 ms, Memory: 39.6 MB - LeetHub

--- next-closest-time/next-closest-time.java
+++ next-closest-time/next-closest-time.java
@@ -1,19 +1,30 @@
-class Solution {
-    public String nextClosestTime(String time) {
-        int cur = 60 * Integer.parseInt(time.substring(0, 2));
-        cur += Integer.parseInt(time.substring(3));
-        Set<Integer> allowed = new HashSet();
-        for (char c: time.toCharArray()) if (c != ':') {
-            allowed.add(c - '0');
-        }
+class Solution{
+	public String nextClosestTime(String time){
+		Set<Integer> digits = new HashSet<Integer>();
+		String answer = "";
 
-        while (true) {
-            cur = (cur + 1) % (24 * 60);
-            int[] digits = new int[]{cur / 60 / 10, cur / 60 % 10, cur % 60 / 10, cur % 60 % 10};
-            search : {
-                for (int d: digits) if (!allowed.contains(d)) break search;
-                return String.format("%02d:%02d", cur / 60, cur % 60);
-            }
-        }
-    }
+		for(char c : time.toCharArray()){
+			digits.add(c - '0');
+		}
+
+		int minutes = Integer.parseInt(time.substring(0, 2)) * 60;
+		minutes += Integer.parseInt(time.substring(3));
+
+		while(true){
+			minutes = (minutes + 1) % (24 * 60);
+			int[] nextTime = {minutes / 60 / 10, minutes / 60 % 10, minutes % 60 / 10, minutes % 60 % 10};
+
+			boolean isValid = true;
+			for(int i : nextTime){
+				if(!digits.contains(i)){
+					isValid = false;
+				}
+			}
+
+			if(isValid){
+				answer = nextTime[0] + "" + nextTime[1] + ":" + nextTime[2] + "" + nextTime[3];
+				return answer;
+			}
+		}
+	}
 }
