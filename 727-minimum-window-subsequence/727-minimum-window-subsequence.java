5b126409fbd8e1da98c3d7f012f292399af4cd6e
bhaleraoyash <65583711+bhaleraoyash@users.noreply.github.com>
2022-01-31 16:01:15 -0500

    Time: 17 ms (65.32%), Space: 45.6 MB (45.82%) - LeetHub

---
+++ 727-minimum-window-subsequence/727-minimum-window-subsequence.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String minWindow(String s1, String s2) {
+        String answer = "";
+        int j = 0;
+        int min = s1.length() + 1;
+        
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == s2.charAt(j)) {
+                j++;
+                if (j == s2.length()) {
+                    int end = i + 1;
+                    j--;
+                    while (j >= 0) {
+                        if (s1.charAt(i) == s2.charAt(j)) j--;
+                        i--;
+                    }
+                    j++;
+                    i++;
+                    if (end - i < min) {
+                        min = end - i;
+                        answer = s1.substring(i, end);
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
