a596be15a773739d9e6d61813e99d2b2a817b87e
bhaleraoyash <65583711+bhaleraoyash@users.noreply.github.com>
2022-02-01 11:04:54 -0500

    Time: 76 ms (16.46%), Space: 119.4 MB (5.15%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,37 +1,30 @@
 class Solution{
 	public int[][] kClosest(int[][] points, int k){
-        int[][] answer = new int[k][2];
-        
-		PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) -> b[0] - a[0]);
-        
-        for(int i = 0; i < points.length; i++){
-            int[] entry = {distance(points[i]), i};
-            if(heap.size() < k){
-                heap.add(entry);
-            }
-            else{
-                if(entry[0] < heap.peek()[0]){
-                    heap.poll();
-                    heap.add(entry);
-                }
-            }
-        }
-        
-        int i = 0;
-        while(!heap.isEmpty()){
-            answer[i] = points[heap.poll()[1]];
-            i++;
-        }
-        return answer;
+		PriorityQueue<int[]> maxHeap = new PriorityQueue<int[]>((a, b) -> b[1] - a[1]);
+		int[][] answer = new int[k][2];
+
+		for(int i = 0; i < points.length; i++){
+			int distance = calculateDistance(points[i][0], points[i][1]);
+            maxHeap.add(new int[]{i, distance});
+			if(maxHeap.size() > k){
+                maxHeap.poll();
+			}
+		}
+
+		int i = 0;
+		while(!maxHeap.isEmpty()){
+			int[] temp = maxHeap.poll();
+			answer[i] = points[temp[0]];
+			i++;
+		}
+		return answer;
 	}
-    
-    public int distance(int[] coords){
-        int x1 = 0;
-        int y1 = 0;
-    
-        int x = (coords[0] - x1) * (coords[0] - x1);
-        int y = (coords[1] - y1) * (coords[1] - y1);
-        
-        return x + y;
-    }
+
+	public int calculateDistance(int x2, int y2){
+		int x1 = 0;
+		int y1 = 0;
+		int x = x2 - x1;
+		int y = y2 - y1;
+		return (x * x) + (y * y);
+	}
 }
