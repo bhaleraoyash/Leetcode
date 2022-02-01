02767fd991ba4efa478384f533e4ed65b3f81035
bhaleraoyash <65583711+bhaleraoyash@users.noreply.github.com>
2022-02-01 05:44:15 -0500

    Time: 191 ms (32.86%), Space: 46.8 MB (77.45%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.java
+++ 2013-detect-squares/2013-detect-squares.java
@@ -1,36 +1,35 @@
-class DetectSquares{
-	List<int[]> points;
-	Map<String, Integer> counter;
-
-	public DetectSquares(){
-		points = new ArrayList<int[]>();
-		counter = new HashMap<String, Integer>();
-	}
-
-	public void add(int[] point){
-		points.add(point);
-		String temp = point[0] + "@" + point[1];
-		counter.put(temp, counter.getOrDefault(temp, 0) + 1);
-	}
-
-	public int count(int[] point){
-		int px = point[0];
-		int py = point[1];
-		int sum = 0;
-
-		for(int[] i : points){
-			int x = i[0];
-			int y = i[1];
-			if(px == x || py == y || (Math.abs(px - x) != Math.abs(py - y))){
-				continue;
-			}
-			sum += counter.getOrDefault(x + "@" + py, 0) * counter.getOrDefault(px + "@" + y, 0);
-		}
-
-		return sum;
-	}
+class DetectSquares {
+    
+    List<int[]> points;
+    Map<String, Integer> counter;
+    
+    public DetectSquares() {
+        points = new ArrayList<int[]>();
+        counter = new HashMap<String, Integer>();
+    }
+    
+    public void add(int[] point) {
+        points.add(point);
+        String temp = point[0] + "@" + point[1];
+        counter.put(temp, counter.getOrDefault(temp, 0) + 1);
+    }
+    
+    public int count(int[] point) {
+        int px = point[0];
+        int py = point[1];
+        int sum = 0;
+        
+        for(int[] p : points){
+            int x = p[0];
+            int y = p[1];
+            if(px == x || py == y || Math.abs(px - x) != Math.abs(py - y)){
+                continue;
+            }
+            sum += counter.getOrDefault(x + "@" + py, 0) * counter.getOrDefault(px + "@" + y, 0);
+        }
+        return sum;
+    }
 }
-
 
 /**
  * Your DetectSquares object will be instantiated and called as such:
