a79b5084b1868b85728fb8139d7f62356ab98c32
bhaleraoyash <65583711+bhaleraoyash@users.noreply.github.com>
2022-02-01 07:25:20 -0500

    Time: 63 ms (99.82%), Space: 50.1 MB (55.15%) - LeetHub

--- 384-shuffle-an-array/384-shuffle-an-array.java
+++ 384-shuffle-an-array/384-shuffle-an-array.java
@@ -1,35 +1,28 @@
-class Solution {
+class Solution{
 
-    int[] original;
-    int[] shuffled;
-    Random random;
-    
-    public Solution(int[] nums) {
-        original = new int[nums.length];
-        shuffled = new int[nums.length];
-        original = nums.clone();
-        shuffled = nums.clone();
-        random = new Random();
-    }
-    
-    public int[] reset() {
-        return original;
-    }
-    
-    public int[] shuffle() {
-        for(int i = shuffled.length - 1; i > 0; i--){
-            int r = random.nextInt(i + 1);
-            int temp = shuffled[i];
-            shuffled[i] = shuffled[r];
-            shuffled[r] = temp;
-        }
-        return shuffled;
-    }
+	int[] original;
+	int[] shuffled;
+	Random random;
+	
+	public Solution(int[] nums){
+		original = new int[nums.length];
+		shuffled = new int[nums.length];
+		original = nums.clone();
+		shuffled = nums.clone();
+		random = new Random();
+	}
+
+	public int[] reset(){
+		return original;
+	}
+
+	public int[] shuffle(){
+		for(int i = shuffled.length - 1; i >= 0; i--){
+			int r = random.nextInt(i + 1);
+			int temp = shuffled[i];
+			shuffled[i] = shuffled[r];
+			shuffled[r] = temp;
+		}
+		return shuffled;
+	}
 }
-
-/**
- * Your Solution object will be instantiated and called as such:
- * Solution obj = new Solution(nums);
- * int[] param_1 = obj.reset();
- * int[] param_2 = obj.shuffle();
- */
