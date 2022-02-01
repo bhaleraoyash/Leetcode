5d55558957b8a8a6cabd1e84ba8d1a8123299dae
bhaleraoyash <65583711+bhaleraoyash@users.noreply.github.com>
2022-02-01 01:29:51 -0500

    Time: 35 ms (73.09%), Space: 42.3 MB (49.76%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,20 +1,20 @@
-class Solution {
-    public int longestStrChain(String[] words) {
-        Map<String, Integer> dp = new HashMap<String, Integer>();
-        int answer = 0;
-        
-        Arrays.sort(words, (a, b) -> a.length() - b.length());
-        
-        for(String word : words){
-            int longestChain = 0;
-            for(int i = 0; i < word.length(); i++){
-                String predecessor = word.substring(0, i) + word.substring(i + 1, word.length());
-                longestChain = Math.max(longestChain, dp.getOrDefault(predecessor, 0) + 1);
-            }
-            dp.put(word, longestChain);
-            answer = Math.max(answer, longestChain);
-        }
-        
-        return answer;
-    }
+class Solution{
+	public int longestStrChain(String[] words){
+		Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+		Map<String, Integer> dp = new HashMap<String, Integer>();
+		int answer = 0;
+
+		for(String s : words){
+			int longestChain = 0;
+			for(int i = 0; i < s.length(); i++){
+				String pred = s.substring(0, i) + s.substring(i + 1);
+				longestChain = Math.max(longestChain, dp.getOrDefault(pred, 0) + 1);
+			}
+			dp.put(s, longestChain);
+			answer = Math.max(answer, longestChain);
+		}
+
+		return answer;
+	}
 }
