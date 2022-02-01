d8ce3a3b3cd937727925d27457246bd41d2f7860
bhaleraoyash <65583711+bhaleraoyash@users.noreply.github.com>
2022-01-31 23:57:33 -0500

    Time: 175 ms (33.75%), Space: 118.1 MB (5.16%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -1,13 +1,13 @@
 class Solution{
 	public int numMatchingSubseq(String s, String[] words){
 		Map<Character, Queue<String>> dict = new HashMap<Character, Queue<String>>();
-        int answer = 0;
+		int answer = 0;
 
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            dict.putIfAbsent(c, new LinkedList());
-        }
-        
+		for(int i = 0; i < s.length(); i++){
+			char c = s.charAt(i);
+			dict.put(c, new LinkedList<String>());
+		}
+
 		for(int i = 0; i < words.length; i++){
 			char c = words[i].charAt(0);
 			if(dict.containsKey(c)){
@@ -17,19 +17,21 @@ class Solution{
 
 		for(int i = 0; i < s.length(); i++){
 			char c = s.charAt(i);
-            Queue<String> q = dict.get(c);
-            int size = q.size();
-            for(int j = 0; j < size; j++){
-                String str = q.poll();
-                if(str.substring(1).length() == 0){
-                    answer++;
-                }
-                else{
-                    if(dict.containsKey(str.charAt(1))){
-                        dict.get(str.charAt(1)).add(str.substring(1));
-                    }
-                }
-            }
+			Queue<String> temp = dict.get(c);
+			int size = temp.size();
+			for(int j = 0; j < size; j++){
+				String s1 = temp.poll();
+				String shortened = s1.substring(1);
+				if(shortened.length() == 0){
+					answer++;
+				}
+				else{
+					char c1 = shortened.charAt(0);
+					if(dict.containsKey(c1)){
+						dict.get(c1).add(shortened);
+					}
+				}
+			}
 		}
 
 		return answer;
