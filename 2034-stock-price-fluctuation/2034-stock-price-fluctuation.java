9f7a3c4c7149e3875ce43ed6121aae6582b8ed2d
bhaleraoyash <65583711+bhaleraoyash@users.noreply.github.com>
2022-02-01 05:57:25 -0500

    Time: 172 ms (58.77%), Space: 160.9 MB (20.20%) - LeetHub

--- 2034-stock-price-fluctuation/2034-stock-price-fluctuation.java
+++ 2034-stock-price-fluctuation/2034-stock-price-fluctuation.java
@@ -1,44 +1,41 @@
-class StockPrice {
+class StockPrice{
+	PriorityQueue<Pair<Integer, Integer>> maxHeap;
+	PriorityQueue<Pair<Integer, Integer>> minHeap;
+	Map<Integer, Integer> map;
+	int latestTimeStamp = 0;
 
-    PriorityQueue<Pair<Integer,Integer>> maxHeap;
-    PriorityQueue<Pair<Integer,Integer>> minHeap;
-    Map<Integer,Integer> map;
-    int lastestTimeStamp = 0;
-    
-    public StockPrice() {
-     maxHeap = new PriorityQueue<Pair<Integer,Integer>>((p1,p2) ->
-                                                         p2.getValue()-p1.getValue());
-         minHeap = new PriorityQueue<Pair<Integer,Integer>>((p1,p2)->
-                                                         p1.getValue()-p2.getValue()); 
-        map = new HashMap();
-    }
-    
-    public void update(int timestamp, int price) {
-        map.put(timestamp,price);
-        maxHeap.add(new Pair<Integer,Integer>(timestamp,price));
-        minHeap.add(new Pair<Integer,Integer>(timestamp,price));
-        lastestTimeStamp = Math.max(lastestTimeStamp,timestamp);
+	public StockPrice(){
+		maxHeap = new PriorityQueue<Pair<Integer, Integer>>((a,b) -> b.getValue() - a.getValue());
+		minHeap = new PriorityQueue<Pair<Integer, Integer>>((a, b) -> a.getValue() - b.getValue());
+		map = new HashMap<Integer, Integer>();
+	}
 
-    }
-    
-    public int current() {
-        return map.get(lastestTimeStamp);
-    }
-    
-    public int maximum() {
-        while(!map.get(maxHeap.peek().getKey()).equals(maxHeap.peek().getValue())){
-          maxHeap.poll();  
-        }
-        return maxHeap.peek().getValue();
-    }
-    
-    public int minimum() {
-      while(!map.get(minHeap.peek().getKey()).equals(minHeap.peek().getValue())){
-          minHeap.poll();  
-        }
-        return minHeap.peek().getValue();   
-    }
+	public void update(int timestamp, int price){
+		map.put(timestamp, price);
+		maxHeap.add(new Pair<Integer, Integer>(timestamp, price));
+		minHeap.add(new Pair<Integer, Integer>(timestamp, price));
+		latestTimeStamp = Math.max(timestamp, latestTimeStamp);
+	}
+
+	public int current(){
+		return map.get(latestTimeStamp);
+	}
+
+	public int maximum(){
+		while(!map.get(maxHeap.peek().getKey()).equals(maxHeap.peek().getValue())){
+		maxHeap.poll();
+		}
+		return maxHeap.peek().getValue();
+	}
+
+	public int minimum(){
+		while(!map.get(minHeap.peek().getKey()).equals(minHeap.peek().getValue())){
+		minHeap.poll();
+		}
+		return minHeap.peek().getValue();
+	}
 }
+
 
 /**
  * Your StockPrice object will be instantiated and called as such:
