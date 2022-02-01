83d58b6b690f6eb4b07bcee9727856be2bb9e5a2
bhaleraoyash <65583711+bhaleraoyash@users.noreply.github.com>
2022-01-31 22:49:10 -0500

    Time: 86 ms (81.33%), Space: 60.8 MB (55.89%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,27 +1,30 @@
-class Solution {
-    public int[] findOriginalArray(int[] changed) {
-        if(changed.length % 2 != 0){
-            return new int[0];
-        }
-        int[] answer = new int[changed.length];
-        Map<Integer, Integer> counter = new HashMap<Integer, Integer>();
+class Solution{
+	public int[] findOriginalArray(int[] changed){
+        int n = changed.length;
+		if(n % 2 != 0){
+			return new int[0];
+		}
         
-        Arrays.sort(changed);
-        
-        int j = 0;
-        for(int i = changed.length - 1; i >= 0; i--){
-            int single = changed[i];
-            int twice = single * 2;
-            if(counter.containsKey(twice) && counter.get(twice) > 0){
-                answer[j] = single;
-                counter.put(twice, counter.get(twice) - 1);
-                j++;
-            }
-            else{
-                counter.put(single, counter.getOrDefault(single, 0) + 1);
-            }
-        }
-        
-        return j == changed.length / 2 ? Arrays.copyOfRange(answer, 0, j) : new int[0];
-    }
+		int[] answer = new int[n];
+		Map<Integer, Integer> counter = new HashMap<Integer, Integer>();
+	
+
+		Arrays.sort(changed);
+		
+		int j = 0;
+		for(int i = n - 1; i >= 0; i--){
+			int single = changed[i];
+			int twice = single * 2;
+
+			if(counter.containsKey(twice) && counter.get(twice) > 0){
+				answer[j++] = single;
+				counter.put(twice, counter.get(twice) - 1);
+			}
+			else{
+				counter.put(single, counter.getOrDefault(single, 0) + 1);
+			}
+		}
+
+		return j == n / 2 ? Arrays.copyOfRange(answer, 0, j) : new int[0];
+	}
 }
