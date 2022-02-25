d186ea8c24f62904a021d886928009431cd0985a
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-02-25 11:45:54 -0500

    Time: 2 ms (88.23%), Space: 67.8 MB (26.55%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,13 +1,13 @@
-class Solution{
-	public boolean canJump(int[] nums){
-		int lastGoodIndex = nums.length - 1;
-
-		for(int i = nums.length - 2; i >= 0; i--){
-			if(i + nums[i] >= lastGoodIndex){
-				lastGoodIndex = i;
-			}
-		}
-
-		return lastGoodIndex == 0;
-	}
+class Solution {
+    public boolean canJump(int[] nums) {
+        int lastGood = nums.length - 1;
+        
+        for(int i = nums.length - 2; i >= 0; i--){
+            if(nums[i] + i >= lastGood){
+                lastGood = i;
+            }
+        }
+        
+        return lastGood == 0;
+    }
 }
