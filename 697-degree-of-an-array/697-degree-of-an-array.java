fbf1d09edb1345b749d8c7a4a8176f33db20a1f7
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-03-02 08:58:54 -0500

    Time: 17 ms (85.82%), Space: 46.3 MB (85.44%) - LeetHub

--- 697-degree-of-an-array/697-degree-of-an-array.java
+++ 697-degree-of-an-array/697-degree-of-an-array.java
@@ -1,30 +1,24 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
         Map<Integer, Integer> counter = new HashMap<Integer, Integer>();
-        Map<Integer, List<Integer>> indices = new HashMap<Integer, List<Integer>>();
+        Map<Integer, Integer> left = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> right = new HashMap<Integer, Integer>();
         int degree = -1;
         int answer = Integer.MAX_VALUE;
         
         for(int i = 0; i < nums.length; i++){
             counter.put(nums[i], counter.getOrDefault(nums[i], 0) + 1);
-            if(indices.containsKey(nums[i])){
-                indices.get(nums[i]).add(i);
+            if(!left.containsKey(nums[i])){
+                left.put(nums[i], i);
             }
-            else{
-                List<Integer> temp = new ArrayList<Integer>();
-                temp.add(i);
-                indices.put(nums[i], temp);
-            }
+            right.put(nums[i], i);
         }
         
         degree = Collections.max(counter.values());
         
-        for(Integer key : indices.keySet()){
+        for(Integer key : nums){
             if(counter.get(key) == degree){
-                List<Integer> temp = indices.get(key);
-                int min = Collections.min(temp);
-                int max = Collections.max(temp);
-                answer = Math.min(answer, (max - min) + 1);
+                answer = Math.min(answer, (right.get(key) - left.get(key)) + 1);
             }
         }
         
