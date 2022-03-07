431c990e1cbc0d17a0b00146312d403b09524745
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-03-07 08:14:11 -0500

    Time: 14 ms (30.78%), Space: 42.3 MB (15.99%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.java
+++ 2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -1,26 +1,28 @@
 class Solution {
     public String capitalizeTitle(String title) {
-        String[] words = title.split(" ");
         String answer = "";
-        StringBuilder sb = new StringBuilder();
+        String[] parts = title.split("\\s+");
         
-        for(int i = 0; i < words.length; i++){
-            String temp = "";
-            if(words[i].length() <= 2){
-                temp = words[i].toLowerCase();
+        for(int i = 0; i < parts.length; i++){
+            if(i == 0){
+                if(parts[i].length() > 2){
+                    String temp = parts[i].toLowerCase();
+                    String first = temp.charAt(0) + "";
+                    answer += first.toUpperCase() + temp.substring(1);
+                }
+                else{
+                    answer += parts[i].toLowerCase();
+                }
             }
             else{
-                temp = words[i].substring(0, 1).toUpperCase() + words[i].substring(1).toLowerCase();
-            }
-            words[i] = temp;
-        }
-        
-        for(int i = 0; i < words.length; i++){
-            if(i != words.length - 1){
-                answer += words[i] + " ";
-            }
-            else{
-                answer += words[i];
+                if(parts[i].length() > 2){
+                    String temp = parts[i].toLowerCase();
+                    String first = temp.charAt(0) + "";
+                    answer += " " + first.toUpperCase() + temp.substring(1);
+                }
+                else{
+                    answer += " " + parts[i].toLowerCase();
+                }
             }
         }
         
