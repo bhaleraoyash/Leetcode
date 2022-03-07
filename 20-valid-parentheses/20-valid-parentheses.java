354200607d201137c93a0b9328f18f68437d9010
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-03-06 20:41:52 -0500

    Time: 2 ms (80.29%), Space: 42.5 MB (11.62%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> brackets = new Stack<Character>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '{' || c == '[' || c == '('){
+                brackets.push(c);
+            }
+            else if(!brackets.isEmpty()){
+                if(c == '}' && brackets.peek() == '{'){
+                    brackets.pop();
+                }
+                else if(c == ')' && brackets.peek() == '('){
+                    brackets.pop();
+                }
+                else if(c == ']' && brackets.peek() == '['){
+                    brackets.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        
+        return brackets.isEmpty() ? true : false;
+    }
+}
