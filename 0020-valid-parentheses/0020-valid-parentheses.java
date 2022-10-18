2f820d7bec3761344733fb8d024fc5a864ec8d1d
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-10-18 19:18:46 -0400

    Time: 2 ms (91.10%), Space: 40 MB (96.76%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,17 @@
 class Solution{
 	public boolean isValid(String s){
         Stack<Character> stack = new Stack<Character>();
+        Map<Character, Character> brackets = new HashMap<Character, Character>();
+        brackets.put('}', '{');
+        brackets.put(')', '(');
+        brackets.put(']', '[');
 
 		for(int i = 0; i < s.length(); i++){
 			char c = s.charAt(i);
 			if(c == '(' || c == '{' || c == '['){
                 stack.push(c);
             }
-            else if(c == ')' && !stack.isEmpty() && stack.peek() == '('){
-                stack.pop();
-            }
-            else if(c == '}' && !stack.isEmpty() && stack.peek() == '{'){
-                stack.pop();
-            }
-            else if(c == ']' && !stack.isEmpty() && stack.peek() == '['){
+            else if(brackets.containsKey(c) && !stack.isEmpty() && brackets.get(c) == stack.peek()){
                 stack.pop();
             }
             else{
