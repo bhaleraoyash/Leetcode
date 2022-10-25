8935f61706254b975e55e1cb729d6c147cbea18a
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-10-24 21:01:26 -0400

    Time: 117 ms (6.87%), Space: 70.5 MB (7.11%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<Character>> strings = new HashMap<String, List<Character>>();
+        Map<List<Character>, List<String>> lists = new HashMap<List<Character>, List<String>>();
+        List<List<String>> answer = new ArrayList<List<String>>();
+        
+        for(int i = 0; i < strs.length; i++){
+            char[] temp = strs[i].toCharArray();
+            List<Character> array_list = new ArrayList<Character>();
+            for(char c : temp){
+                array_list.add(c);
+            }
+            Collections.sort(array_list);
+            strings.put(strs[i], array_list);
+            
+        }
+        System.out.println(strings);
+        
+        for(String s1 : strs){
+            List<Character> chars = strings.get(s1);
+            System.out.println(s1);
+            if(lists.containsKey(chars)){
+                List<String> temp = lists.get(chars);
+                temp.add(s1);
+                lists.put(chars, temp);
+            }
+            else{
+                List<String> temp = new ArrayList<String>();
+                temp.add(s1);
+                lists.put(chars, temp);
+            }
+        }
+        
+        for(List<Character> c : lists.keySet()){
+            answer.add(lists.get(c));
+        }
+        
+        return answer;
+    }
+}
