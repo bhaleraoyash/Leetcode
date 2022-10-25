9c95d370e5b90b7c6e545e3583384b6c1684193e
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-10-24 21:02:54 -0400

    Time: 6 ms (99.59%), Space: 45.3 MB (93.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,39 +1,14 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<Character>> strings = new HashMap<String, List<Character>>();
-        Map<List<Character>, List<String>> lists = new HashMap<List<Character>, List<String>>();
-        List<List<String>> answer = new ArrayList<List<String>>();
-        
-        for(int i = 0; i < strs.length; i++){
-            char[] temp = strs[i].toCharArray();
-            List<Character> array_list = new ArrayList<Character>();
-            for(char c : temp){
-                array_list.add(c);
-            }
-            Collections.sort(array_list);
-            strings.put(strs[i], array_list);
-            
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
         }
-        
-        for(String s1 : strs){
-            List<Character> chars = strings.get(s1);
-            System.out.println(s1);
-            if(lists.containsKey(chars)){
-                List<String> temp = lists.get(chars);
-                temp.add(s1);
-                lists.put(chars, temp);
-            }
-            else{
-                List<String> temp = new ArrayList<String>();
-                temp.add(s1);
-                lists.put(chars, temp);
-            }
-        }
-        
-        for(List<Character> c : lists.keySet()){
-            answer.add(lists.get(c));
-        }
-        
-        return answer;
+        return new ArrayList(ans.values());
     }
 }
