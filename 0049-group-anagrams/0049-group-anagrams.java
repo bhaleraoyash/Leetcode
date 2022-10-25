7ced355bf08c3484cd511d043f409dfb9db4429c
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-10-24 21:15:50 -0400

    Time: 13 ms (75.19%), Space: 55.1 MB (79.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,38 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<Character>> strings = new HashMap<String, List<Character>>();
-        Map<List<Character>, List<String>> lists = new HashMap<List<Character>, List<String>>();
-        List<List<String>> answer = new ArrayList<List<String>>();
+        if(strs.length == 0) return new ArrayList();
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
         
-        for(int i = 0; i < strs.length; i++){
-            char[] temp = strs[i].toCharArray();
-            List<Character> array_list = new ArrayList<Character>();
-            for(char c : temp){
-                array_list.add(c);
+        for(String s : strs){
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String temp = String.valueOf(c);
+            if(!map.containsKey(temp)){
+                map.put(temp, new ArrayList());
             }
-            Collections.sort(array_list);
-            strings.put(strs[i], array_list);
-            
+            map.get(temp).add(s);
         }
         
-        for(String s1 : strs){
-            List<Character> chars = strings.get(s1);
-            if(lists.containsKey(chars)){
-                List<String> temp = lists.get(chars);
-                temp.add(s1);
-                lists.put(chars, temp);
-            }
-            else{
-                List<String> temp = new ArrayList<String>();
-                temp.add(s1);
-                lists.put(chars, temp);
-            }
-        }
-        
-        for(List<Character> c : lists.keySet()){
-            answer.add(lists.get(c));
-        }
-        
-        return answer;
+        return new ArrayList(map.values());
     }
 }
