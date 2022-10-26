f53baf7b25ed074f1e713ff3cc7911bf34a3a7cb
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-10-26 09:03:11 -0400

    Time: 155 ms (15.05%), Space: 117.3 MB (6.37%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        Stack<Pair<Character, Integer>> stack = new Stack<Pair<Character, Integer>>();
+        String answer = "";
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(stack.isEmpty()){
+                Pair<Character, Integer> temp = new Pair<Character, Integer>(c, 1);
+                stack.push(temp);
+            }
+            else{
+                if(stack.peek().getKey() == c){
+                    Pair<Character, Integer> temp1 = stack.pop();
+                    Pair<Character, Integer> temp2 = new Pair<Character, Integer>(c, temp1.getValue() + 1);
+                    if(temp2.getValue() < k){
+                        stack.push(temp2);
+                    }
+                }
+                else{
+                    Pair<Character, Integer> temp = new Pair<Character, Integer>(c, 1);
+                    stack.push(temp);
+                }
+            }
+        }
+        
+        while(!stack.isEmpty()){
+            Pair<Character, Integer> temp = stack.pop();
+            String str = String.valueOf(temp.getKey());
+            String current = str.repeat(temp.getValue());
+            answer = current + answer;
+        }
+        
+        return answer;
+    }
+}
