6f3aa67879229b3fa4742103f548e7f1722da895
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-10-30 11:59:58 -0400

    Time: 0 ms (100.00%), Space: 42.5 MB (71.83%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index1 = m - 1;
+        int index2 = n - 1;
+        int slider = nums1.length - 1;
+        
+        while(slider >= 0){
+            int firstElement = index1 >= 0 ? nums1[index1] : Integer.MIN_VALUE;
+            int secondElement = index2 >= 0 ? nums2[index2] : Integer.MIN_VALUE;
+            if(firstElement > secondElement){
+                nums1[slider] = firstElement;
+                slider--;
+                index1--;
+            }
+            else{
+                nums1[slider] = secondElement;
+                slider--;
+                index2--;
+            }
+        }
+    }
+}
