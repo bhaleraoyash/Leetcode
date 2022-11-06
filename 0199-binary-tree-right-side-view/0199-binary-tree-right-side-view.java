c77ebc293e44ebbb6ccfd79f67a71059653772b3
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-11-06 16:59:20 -0500

    Time: 2 ms (55.42%), Space: 42.6 MB (63.79%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,29 +1,44 @@
-class Solution{
-	public List<Integer> rightSideView(TreeNode root){
-		List<Integer> answer = new ArrayList<Integer>();
-		Queue<TreeNode> q = new LinkedList<TreeNode>();
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> answer = new ArrayList<Integer>();
         if(root == null){
             return answer;
         }
-		q.offer(root);
-
-		while(!q.isEmpty()){
-            int level = q.size();
-			for(int i = 0; i < level; i++){
-				TreeNode temp = q.poll();
-				if(i == level - 1){
-					answer.add(temp.val);
-				}
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                TreeNode temp = queue.remove();
+                if(i == size - 1){
+                    answer.add(temp.val);
+                }
                 if(temp.left != null){
-					q.offer(temp.left);
-				}
-				if(temp.right != null){
-					q.offer(temp.right);
-				}
-			}
-		}
-		
-		return answer;
-	}
+                    queue.add(temp.left);
+                }
+                if(temp.right != null){
+                    queue.add(temp.right);
+                }
+            }
+        }
+        
+        return answer;
+    }
 }
