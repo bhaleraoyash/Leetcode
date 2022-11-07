1c6fc4f6f11a1f36d498afea39dcb96ce9d2a416
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-11-07 05:42:34 -0500

    Time: 1 ms (100.00%), Space: 43.1 MB (94.42%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,22 +1,23 @@
-class Solution{
-	public List<String> fizzBuzz(int n){
-		List<String> answer = new ArrayList<String>();
-
-		for(int i = 1; i <= n; i++){
-			if(i % 3 == 0 && i % 5 == 0){
-				answer.add("FizzBuzz");
-			}
-			else if(i % 3 == 0){
-				answer.add("Fizz");
-			}
-			else if(i % 5 == 0){
-				answer.add("Buzz");
-			}
-			else{
-				answer.add(String.valueOf(i));
-			}
-		}
-		
-		return answer;
-	}
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<String>();
+        
+        for(int i = 0; i < n; i++){
+            int num = i + 1;
+            if(num % 3 == 0 && num % 5 == 0){
+                answer.add("FizzBuzz");
+            }
+            else if(num % 3 == 0){
+                answer.add("Fizz");
+            }
+            else if(num % 5 == 0){
+                answer.add("Buzz");
+            }
+            else{
+                answer.add(String.valueOf(num));
+            }
+        }
+        
+        return answer;
+    }
 }
