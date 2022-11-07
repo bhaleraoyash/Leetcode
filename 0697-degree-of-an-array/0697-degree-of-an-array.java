3a222747abba06e826a4b6ad09e50ca8e0550502
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-11-07 00:13:36 -0500

    Time: 21 ms (88.15%), Space: 48.1 MB (83.65%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -1,33 +1,28 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
         Map<Integer, Integer> counter = new HashMap<Integer, Integer>();
-        Map<Integer, List<Integer>> indices = new HashMap<Integer, List<Integer>>();
+        Map<Integer, List<Integer>> index = new HashMap<Integer, List<Integer>>();
         int answer = Integer.MAX_VALUE;
         
         for(int i = 0; i < nums.length; i++){
             counter.put(nums[i], counter.getOrDefault(nums[i], 0) + 1);
-            if(!indices.containsKey(nums[i])){
-                List<Integer> temp = new ArrayList<Integer>();
-                temp.add(i);
-                indices.put(nums[i], temp);
+            if(index.containsKey(nums[i])){
+                index.get(nums[i]).add(i);
             }
             else{
-                List<Integer> temp = indices.get(nums[i]);
+                List<Integer> temp = new ArrayList<Integer>();
                 temp.add(i);
-                indices.put(nums[i], temp);
+                index.put(nums[i], temp);
             }
         }
         
-        int max = Integer.MIN_VALUE;
+        int max = Collections.max(counter.values());
         
-        for(Integer i : counter.keySet()){
-            max = Math.max(max, counter.get(i));   
-        }
-        
-        for(Integer i : indices.keySet()){
-            List<Integer> temp = indices.get(i);
-            if(temp.size() == max){
-                answer = Math.min(answer, (temp.get(temp.size() - 1) - temp.get(0)) + 1);
+        for(int i : index.keySet()){
+            if(counter.get(i) == max){
+                List<Integer> temp = index.get(i);
+                int current = temp.get(temp.size() - 1) - temp.get(0) + 1;
+                answer = Math.min(current, answer);
             }
         }
         
