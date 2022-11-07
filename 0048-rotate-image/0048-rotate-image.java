2b2967e87c704c3fdfe6d44a472dedae5f71e766
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-11-07 05:51:06 -0500

    Time: 0 ms (100.00%), Space: 42.7 MB (62.31%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,6 +1,5 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        //Transpose
         for(int i = 0; i < matrix.length; i++){
             for(int j = i; j < matrix[i].length; j++){
                 int temp = matrix[i][j];
@@ -9,10 +8,9 @@ class Solution {
             }
         }
         
-        //Reflect
         for(int i = 0; i < matrix.length; i++){
             int left = 0;
-            int right = matrix[0].length - 1;
+            int right = matrix[i].length - 1;
             while(left < right){
                 int temp = matrix[i][left];
                 matrix[i][left] = matrix[i][right];
