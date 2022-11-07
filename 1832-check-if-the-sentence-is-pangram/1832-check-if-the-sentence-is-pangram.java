5a3af415989240d83656edf6968f6aa2001253bb
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-11-06 20:01:32 -0500

    Time: 9 ms (17.49%), Space: 41.9 MB (64.17%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,14 +1,12 @@
-class Solution{
-	public boolean checkIfPangram(String sentence){
-		Set<Character> chars = new HashSet<Character>();
-
-		for(int i = 0; i < sentence.length(); i++){
-			char c = sentence.charAt(i);
-			if(Character.isLetter(c)){
-				chars.add(c);
-			}
-		}
-
-		return chars.size() == 26;
-	}
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        Set<Character> alphabets = new HashSet<Character>();
+        
+        for(int i = 0; i < sentence.length(); i++){
+            char c = sentence.charAt(i);
+            alphabets.add(c);
+        }
+        
+        return alphabets.size() == 26;
+    }
 }
