423f646fa5862e298c31279806d3171a14c88abe
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-11-06 20:31:20 -0500

    Time: 3 ms (97.62%), Space: 57.8 MB (22.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,28 +1,29 @@
-class Solution{
-	public int numIslands(char[][] grid){
-		int answer = 0;
-
-		for(int i = 0; i < grid.length; i++){
-			for(int j = 0; j < grid[i].length; j++){
-				if(grid[i][j] == '1'){
-					answer += 1;
-					changeIsland(grid, i, j);
-				}
-			}
-		}
-
-		return answer;
-	}
-
-	public void changeIsland(char[][] grid, int i, int j){
-		if(i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0'){
-			return;
-		}
-
-		grid[i][j] = '0';
-		changeIsland(grid, i - 1, j);
-		changeIsland(grid, i + 1, j);
-		changeIsland(grid, i, j - 1);
-		changeIsland(grid, i, j + 1);
-	}
+class Solution {
+    public int numIslands(char[][] grid) {
+        int answer = 0;
+        
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                if(grid[i][j] == '1'){
+                    answer++;
+                    convertIsland(grid, i, j);
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    public void convertIsland(char[][] grid, int i, int j){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0'){
+            return;
+        }
+        
+        grid[i][j] = '0';
+        
+        convertIsland(grid, i - 1, j);
+        convertIsland(grid, i + 1, j);
+        convertIsland(grid, i, j - 1);
+        convertIsland(grid, i, j + 1);
+    }
 }
