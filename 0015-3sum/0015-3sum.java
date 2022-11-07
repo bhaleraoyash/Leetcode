34487f4e7ea64aaf8c1bec4aaf7c3d3633d36d51
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-11-07 10:07:41 -0500

    Time: 560 ms (25.90%), Space: 146 MB (9.09%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,37 @@
-class Solution{
-	public List<List<Integer>> threeSum(int[] nums){
-		Set<List<Integer>> answer = new HashSet<List<Integer>>();
-
-		Arrays.sort(nums);
-
-		for(int i = 0; i < nums.length; i++){
-			int left = i + 1;
-			int right = nums.length - 1;
-			while(left < right){
-				int sum = nums[i] + nums[left] + nums[right];
-				if(sum > 0){
-					right--;
-				}
-				else if(sum < 0){
-					left++;
-				}
-				else{
-					List<Integer> temp = new ArrayList<Integer>();
-					temp.add(nums[i]);
-					temp.add(nums[left]);
-					temp.add(nums[right]);
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> answer = new HashSet<List<Integer>>();
+        List<List<Integer>> solution = new ArrayList<List<Integer>>();
+        
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < nums.length; i++){
+            int low = i + 1;
+            int high = nums.length - 1;
+            while(low < high){
+                int sum = nums[i] + nums[low] + nums[high];
+                if(sum == 0){
+                    List<Integer> temp = new ArrayList<Integer>();
+                    temp.add(nums[i]);
+                    temp.add(nums[low]);
+                    temp.add(nums[high]);
                     answer.add(temp);
-                    left++;
-                    right--;
-				}
-			}
-		}
-
-		return new ArrayList(answer);
-	}
+                    low++;
+                    high--;
+                }
+                else if(sum < 0){
+                    low++;
+                }
+                else{
+                    high--;
+                }
+            }
+        }
+        
+        for(List<Integer> list : answer){
+            solution.add(list);
+        }
+        
+        return solution;
+    }
 }
