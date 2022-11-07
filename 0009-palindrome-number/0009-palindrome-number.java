6a74f5a15222199b64401831cd8bc68f5fd0e6d8
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-11-06 20:09:03 -0500

    Time: 9 ms (97.36%), Space: 41.6 MB (91.49%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        
         String s = String.valueOf(x);
         int left = 0;
         int right = s.length() - 1;
         
         while(left <= right){
-            if(s.charAt(left) == s.charAt(right)){
-                left++;
-                right--;
-            }
-            else{
+            if(s.charAt(left) != s.charAt(right)){
                 return false;
             }
+            left++;
+            right--;
         }
         
         return true;
