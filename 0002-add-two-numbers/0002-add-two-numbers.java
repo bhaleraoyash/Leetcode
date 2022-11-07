de4ffe27be97f7f1453e8966ad30830e7449bef3
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-11-06 19:44:46 -0500

    Time: 3 ms (81.58%), Space: 47.5 MB (70.99%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,31 +8,38 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution{
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2){
-		ListNode answer = new ListNode();
-		ListNode dummy = answer;
-		int sum = 0;
-
-		while(l1 != null || l2 != null){
-			if(l1 != null){
-				sum += l1.val;
-				l1 = l1.next;
-			}
-			if(l2 != null){
-				sum += l2.val;
-				l2 = l2.next;
-			}
-			dummy.next = new ListNode(sum % 10);
-			dummy = dummy.next;
-
-			sum = sum > 9 ? 1 : 0;
-		}
-
-		if(sum > 0){
-			dummy.next = new ListNode(sum);
-		}
-		
-		return answer.next;
-	}
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode l3 = dummy;
+        int carry = 0;
+        
+        while(l1 != null || l2 != null){
+            int l1_val = l1 == null? 0 : l1.val;
+            int l2_val = l2 == null? 0 : l2.val;
+            
+            int sum = l1_val + l2_val + carry;
+            carry = sum / 10;
+            int last_digit = sum % 10;
+            
+            ListNode temp = new ListNode(last_digit);
+            l3.next = temp;
+            
+            if(l1 != null){
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                l2 = l2.next;
+            }
+            l3 = l3.next;
+        }
+        
+        if(carry > 0){
+            ListNode temp = new ListNode(carry);
+            l3.next = temp;
+            l3 = l3.next;
+        }
+        
+        return dummy.next;
+    }
 }
