8da39e38419ea8543a841d32350b97ca79e87700
Yash Bhalerao <65583711+bhaleraoyash@users.noreply.github.com>
2022-11-06 20:15:30 -0500

    Time: 4 ms (88.85%), Space: 52.6 MB (91.33%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,20 +1,21 @@
-class Solution{
-	public int maxArea(int[] height){
-		int left = 0;
-		int right = height.length - 1;
-		int answer = 0;
-
-		while(left <= right){
-			int area = (right - left) * Math.min(height[left], height[right]);
-			if(height[left] < height[right]){
-				left++;
-			}
-			else{
-				right--;
-			}
-			answer = Math.max(answer, area);
-		}
-
-		return answer;
-	}
+class Solution {
+    public int maxArea(int[] height) {
+        int answer = 0;
+        int left = 0;
+        int right = height.length - 1;
+        
+        while(left < right){
+            int area = Math.min(height[left], height[right]) * (right - left);
+            answer = Math.max(area, answer);
+            
+            if(height[left] < height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        
+        return answer;
+    }
 }
